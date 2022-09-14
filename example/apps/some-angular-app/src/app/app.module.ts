08f783356f3bd8c51cf450890528a59115d03977
Orjan De Smet <orjan.de.smet@gmail.com>
2022-09-14 10:20:24 +0200

    Update dependencies

--- example/apps/some-angular-app/src/app/app.module.ts
+++ example/apps/some-angular-app/src/app/app.module.ts
@@ -3,17 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
-
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([
-      {
-        path: 'sub',
-        loadChildren: () => import('@example/ng-lib').then(l => l.NgLibModule)
-      }
-    ], { initialNavigation: 'enabled' }),
+    RouterModule.forRoot(
+      [
+        {
+          path: 'sub',
+          loadChildren: () =>
+            import('@example/ng-lib').then((l) => l.NgLibModule),
+        },
+      ],
+      { initialNavigation: 'enabledBlocking' }
+    ),
   ],
   providers: [],
   bootstrap: [AppComponent],
